ad828a7b44aa5dae9330657c9414543952627501
Jhonatan Romero <kollon01@gmail.com>
2020-08-14 12:10:00 -0400

    seed.js error solucioando import de Services model

--- OwlEvents-Api/config/seed.js
+++ OwlEvents-Api/config/seed.js
@@ -1,7 +1,7 @@
 const Classification = require('../models').Classification;
 const classificationSeed = require("../seeds/classification.seed");
 
-const Services = require('../models').Services;
+const Services = require('../models').Service;
 const ServicesSeed = require("../seeds/services.seed");
 //esto se va a mejorar luego. Por ahora hagamos un metodo para caa uno
 const createSeeds = async () => {
