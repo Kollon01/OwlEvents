0ab7863324c9ab00c9613686e0cec96b98dc95de
Jhonatan Romero <kollon01@gmail.com>
2020-08-14 16:10:11 -0400

    Manejo de Semillas (clasificacion y Service) y ya se selecciona aleatoreamente el id de una Clasificacion para un servicio

--- OwlEvents-Api/config/seed.js
+++ OwlEvents-Api/config/seed.js
@@ -1,14 +1,39 @@
-const Classification = require('../models').Classification;
+const Classification = require("../models").Classification;
 const classificationSeed = require("../seeds/classification.seed");
 
-const Services = require('../models').Service;
+const Services = require("../models").Service;
 const ServicesSeed = require("../seeds/services.seed");
+//Funcion para id random 
+function IdRandom() {
+  return Math.ceil(Math.random() * (classificationSeed.length - 0) + 0) - 1;
+}
 //esto se va a mejorar luego. Por ahora hagamos un metodo para caa uno
 const createSeeds = async () => {
-     await Classification.bulkCreate(classificationSeed);
-     //await Services.bulkCreate(ServicesSeed);
-}
+  var idList = [];
+  await Classification.bulkCreate(classificationSeed, {
+    returning: true,
+    raw: true,
+  })
+    .then(function (rs) {
+      rs.forEach((element) => {
+        idList.push({
+          Id: element.dataValues.id,
+        });
+      });
+    })
+    .then(function (rs) {
+      ServicesSeed.forEach((serv, index) => {
+        ServicesSeed[index].ClassificationId = idList[IdRandom()].Id;
+      });
+    })
+    .catch(function (error) {
+      console.log("Error during Post: " + error);
+    });
+  await Services.bulkCreate(ServicesSeed).catch(function (error) {
+    console.log("Error during Post: " + error);
+  });
+};
 
 module.exports.seedDB = () => {
-    createSeeds(); 
-}
+  createSeeds();
+};
--- OwlEvents-Api/seeds/services.seed.js
+++ OwlEvents-Api/seeds/services.seed.js
@@ -1,4 +1,5 @@
 const Services = [
     { name: 'Fiestas y Sorpresas', title: "Contactanos para fiestas", body: "Prestamos nuesros servicios de cocina, instalaciones, seguridad y transporte para fiestas y eventos sociales"},
+    { name: 'Fiestas y Sorpresas', title: "Contactanos para fiestas", body: "Prestamos nuesros servicios de cocina, instalaciones, seguridad y transporte para fiestas y eventos sociales"},
 ]; 
 module.exports = Services;
