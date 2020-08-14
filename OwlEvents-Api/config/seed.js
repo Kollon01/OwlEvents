d981801432859b0f2f2ba090744b5f5cba9a97b1
Enmanuel Marval <emarval.fc@gmail.com>
2020-08-14 18:46:09 -0400

    refactor de seed.js separando los procesos para modificarlos mas facilmente

--- OwlEvents-Api/config/seed.js
+++ OwlEvents-Api/config/seed.js
@@ -1,37 +1,35 @@
+const _ = require('lodash');
+
 const Classification = require("../models").Classification;
 const classificationSeed = require("../seeds/classification.seed");
 
 const Services = require("../models").Service;
 const ServicesSeed = require("../seeds/services.seed");
-//Funcion para id random 
-function IdRandom() {
-  return Math.ceil(Math.random() * (classificationSeed.length - 0) + 0) - 1;
-}
-//esto se va a mejorar luego. Por ahora hagamos un metodo para caa uno
-const createSeeds = async () => {
-  var idList = [];
-  await Classification.bulkCreate(classificationSeed, {
-    returning: true,
-    raw: true,
-  })
-    .then(function (rs) {
-      rs.forEach((element) => {
-        idList.push({
-          Id: element.dataValues.id,
-        });
-      });
-    })
-    .then(function (rs) {
-      ServicesSeed.forEach((serv, index) => {
-        ServicesSeed[index].ClassificationId = idList[IdRandom()].Id;
-      });
-    })
-    .catch(function (error) {
-      console.log("Error during Post: " + error);
+
+const seedClassification = async () => {
+    return Classification.bulkCreate(classificationSeed, {
+        returning: true,
+        raw: true,
     });
-  await Services.bulkCreate(ServicesSeed).catch(function (error) {
-    console.log("Error during Post: " + error);
-  });
+}
+
+const seedServices = async (ServicesSeed) => {
+    return Services.bulkCreate(ServicesSeed);
+}
+
+const getRandomModel = (modelCollection) => {
+    return _.shuffle(modelCollection)[0];
+}
+
+const createSeeds = async () => {
+
+    let classifications = await seedClassification();
+
+    ServicesSeed.forEach((serv, index) => {
+        ServicesSeed[index].ClassificationId = getRandomModel(classifications).id;
+    });
+
+    await seedServices(ServicesSeed);
 };
 
 module.exports.seedDB = () => {
