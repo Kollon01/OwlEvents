0436b54eb3ebc700069781e958952a545bfba1cc
Jhonatan Romero <kollon01@gmail.com>
2020-08-14 10:36:15 -0400

    cambios

--- OwlEvents-Api/app.js
+++ OwlEvents-Api/app.js
@@ -4,9 +4,6 @@ const express                     = require('express');
 const logger                    = require('morgan');
 const bodyParser                = require('body-parser');
 
- 
-
-
 const http = require('http');
 // Set up the express app
 const app = express();
@@ -17,13 +14,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 require('./routes')(app);
 
 //Routes
 require("./routes/usuario.route.js")(app);
 require("./routes/juego.route.js")(app);
-
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
