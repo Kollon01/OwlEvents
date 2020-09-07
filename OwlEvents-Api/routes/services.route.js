21a9974dc95923034e9cdf2516c348b423c6fa98
Enmanuel Marval <emarval.fc@gmail.com>
2020-09-07 09:25:04 -0400

    Eliminado backslash de la ruta. Refactorizado ejemplo de fetch en main page

--- OwlEvents-Api/routes/services.route.js
+++ OwlEvents-Api/routes/services.route.js
@@ -6,5 +6,5 @@ module.exports = function (app) {
    app.post('/api/services/create', serviceController.create);
    app.delete('/api/services/:id', serviceController.get);
    app.get('/api/services/:id', serviceController.get);
-   app.get('/api/services/', serviceController.find);
+   app.get('/api/services', serviceController.find);
 };
--- owleventsfront/src/pages/mainPage.js
+++ owleventsfront/src/pages/mainPage.js
@@ -1,33 +1,32 @@
 
 import React, {useState, useEffect} from 'react';
+import { useQuery } from 'react-query'
 import httpInstance from "../services/http.service";
 
 const MainPage = () => {
-    
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
-        getServices();
+    const { isLoading, error, data } = useQuery('repoData', () =>
+        fetch(`http://localhost:8001/api/services`).then(res =>  res.json())
+    )
 
-    }, []);
-  
-    const getServices = async () => {
-        const serv = await httpInstance.get(`http://localhost:8001/api/services/all`);
-        setData(serv.data);
-        console.log(serv.data, "la data");
-    }
+    if (isLoading) return 'Cargando...'
+
+    if (error) return 'Ha ocurrido un error: ' + error.message
+
+    let {totalItems, services, totalPages, currentPage} = data;
 
     return (
+
         <div className="App">
             <ul>
             {
-                data.map(item => (
-                <li key={item.id}>{item.title} - {item.body}</li>
+                services.map(service => (
+                    <li key={service.id}>{service.title} - {service.body}</li>
                 ))
             }
             </ul>
         </div>
-    );
+    )
 }
 
 export default MainPage;
